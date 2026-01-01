afd0ac43692a433e659c1cd3a28c2e6a025f3e09
Sourav Sanjay <souravsanjay07@gmail.com>
2026-01-01 20:18:36 +0530

    API calls handled and made the UI adaptive to API data

--- Ecom-Cards-Axios/src/App.jsx
+++ Ecom-Cards-Axios/src/App.jsx
@@ -1,27 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import Card from './components/Card'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Card from "./components/Card";
+import axios from "axios";
 
 const App = () => {
-
   const [data, setData] = useState([]);
 
-  let productData = async()=>{
-    let response = await axios.get("https://fakestoreapi.com/products");
-    setData(response.data);
-    console.log(data);
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
+    let productData = async () => {
+      let response = await axios.get("https://fakestoreapi.com/products");
+      setData(response.data);
+      console.log(response.data);
+    };
     productData();
-  },[])
-
+  }, []);
 
   return (
-    <div>
-      <Card/>
+    <div className="flex flex-wrap m-10 gap-3">
+      {data.map((item, idx) => {
+        return (
+          <div key={idx}>
+            {" "}
+            <Card
+              title={item.title}
+              price={item.price}
+              img={item.image}
+              rating={item.rating.rate}
+              count={item.rating.count}
+            />{" "}
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Ecom-Cards-Axios/src/components/Card.jsx
+++ Ecom-Cards-Axios/src/components/Card.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className='m-10 flex flex-wrap gap-3'>
-        <div className="card bg-white w-77 p-8 rounded-3xl flex flex-col gap-5 justify-center items-center">
-            <img className='w-60 rounded-xl' src="https://images.unsplash.com/photo-1766654271534-575410ad1468?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            <div className="infos flex flex-col gap-1">
-                <h1 className='text-2xl text-gray-600 font-semibold'>Dunk High Sneakers</h1>
-                <div className="ratings flex gap-5">
-                    <h3 className='px-2 py-1 bg-gray-200 rounded-xl text-sm font-medium'><i className="ri-star-fill text-yellow-500"></i> 3.9</h3>
-                    <h3 className='px-2 py-1 bg-gray-200 rounded-xl text-sm font-medium'><i class="ri-stack-fill text-blue-400"></i> - 120</h3>
-                </div>
-            </div>
-            <div className="bottom flex justify-between items-center mt-4 w-full gap-4">
-                <div className="price">
-                    <h3 className='text-sm text-gray-500'>Price</h3>
-                    <h1 className='text-xl font-semibold'>$120.00</h1>
-                </div>
-                <div className="button bg-[#2C2C2C] text-white px-3 py-3 rounded-3xl w-full text-center">Buy Now</div>
-            </div>
+    <div className="">
+      <div className="card bg-white h-120 w-77 p-8 rounded-3xl flex flex-col gap-5 justify-around items-center">
+        <img
+          className="w-40 h-40 rounded-xl bg-center bg-cover"
+          src={props.img}
+          alt=""
+        />
+        <div className="infos flex flex-col gap-1">
+          <h1 className="text-xl text-gray-600 font-semibold">{props.title}</h1>
+          <div className="ratings flex gap-5">
+            <h3 className="px-2 py-1 bg-gray-200 rounded-xl text-sm font-medium">
+              <i className="ri-star-fill text-yellow-500"></i> {props.rating}
+            </h3>
+            <h3 className="px-2 py-1 bg-gray-200 rounded-xl text-sm font-medium">
+              <i class="ri-stack-fill text-blue-400"></i> {props.count}
+            </h3>
+          </div>
         </div>
+        <div className="bottom flex justify-between items-center mt-4 w-full gap-4">
+          <div className="price">
+            <h3 className="text-sm text-gray-500">Price</h3>
+            <h1 className="text-xl font-semibold">${props.price}</h1>
+          </div>
+          <div className="button bg-[#2C2C2C] text-white px-3 py-3 rounded-3xl w-full text-center cursor-pointer">
+            Buy Now
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
